799ebd0556600ed7e89dd2c946591eb94034f80c
antifreezzz <vlaklementev@yandex.ru>
2018-06-08 12:16:05 +0300

    GroupModificationTests

---
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupModificationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupModificationTests :TestBase
+    {
+        [Test]
+
+        public void GroupModificationTest()
+        {
+            GroupData newData = new GroupData("111");
+            newData.Header = "222";
+            newData.Footer = "333";
+
+            app.Groups.Modify(1, newData);
+
+        }
+
+    }
+}
