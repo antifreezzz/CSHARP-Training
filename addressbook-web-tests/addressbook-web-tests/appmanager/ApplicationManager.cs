e95531c96800586361b7b99ce3858b39014350a5
antifreezzz <vlaklementev@yandex.ru>
2018-06-08 10:14:23 +0300

    Appmanager и TestBase

---
+++ addressbook-web-tests/addressbook-web-tests/appmanager/ApplicationManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    public class ApplicationManager
+    {
+        protected LoginHelper loginHelper;
+        protected NavigationHelper navigator;
+        protected GroupHelper groupHelper;
+        protected ContactHelper contactHelper;
+
+        protected IWebDriver driver;
+        protected string baseURL;
+
+        public ApplicationManager()
+        {
+            loginHelper = new LoginHelper(driver);
+            navigator = new NavigationHelper(driver, baseURL);
+            groupHelper = new GroupHelper(driver);
+            contactHelper = new ContactHelper(driver); 
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = @"D:\Program Files (x86)\Mozilla Firefox4532\firefox.exe";
+            options.UseLegacyImplementation = true;
+            driver = new FirefoxDriver(options);
+            baseURL = "http://localhost/";
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        public LoginHelper Auth
+        {
+            get
+            {
+                return loginHelper;
+            }
+        }
+
+        public NavigationHelper Navigator
+        {
+            get
+            {
+                return navigator;
+            }
+        }
+
+        public GroupHelper groups
+        {
+            get
+            {
+                return groupHelper;
+            }
+        }
+
+        public ContactHelper contacts
+        {
+            get
+            {
+                return contactHelper;
+            }
+        }
+
+    }
+}
